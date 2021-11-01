d9e9fa6ff2157a4b3ff86a5f6d966920c5387713
yoga-saputra <0yogasaputra@gmail.com>
2021-11-01 21:32:46 +0700

    testing 2

--- seeders/20211030181323-user-seeders.js
+++ seeders/20211030181323-user-seeders.js
@@ -5,14 +5,14 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('users', [
       {
-        name: 'Yoga',
+        name: 'admin',
         role: 'admin',
         password: await bcrypt.hash('admin123', 10),
         created_at: new Date(),
         updated_at: new Date()
       },
       {
-        name: 'ajeng',
+        name: 'user',
         role: 'user',
         password: await bcrypt.hash('user123', 10),
         created_at: new Date(),
